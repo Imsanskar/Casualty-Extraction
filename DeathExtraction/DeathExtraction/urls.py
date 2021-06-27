57f0be70f7558e409cc541d0f3cb0aac1eb8a800
Sanskar Amgain <sanskaramgain@gmail.com>
2021-06-27 17:05:07 +0545

    Created a new app and ofc hello world

--- DeathExtraction/DeathExtraction/urls.py
+++ DeathExtraction/DeathExtraction/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+import newsextraction
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('newsextraction.urls'))
 ]
---
+++ DeathExtraction/newsextraction/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NewsextractionConfig(AppConfig):
+    name = 'newsextraction'
