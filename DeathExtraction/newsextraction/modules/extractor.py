5f0ee9ab9bd61833d97178aa0807676be243fffb
Sanskar Amgain <sanskaramgain@gmail.com>
2021-07-02 19:22:30 +0545

    Minor change: Code refactored

--- DeathExtraction/newsextraction/modules/extractor.py
+++ DeathExtraction/newsextraction/modules/extractor.py
@@ -1,29 +1,33 @@
 #TODO: create a class called DataExtractor to extract key information from the given news
+from .tagger import Tagger
+from .tokenizer import Tagger
 
 
 class DataExtractor:
-    def __init__(self, news:str):
-        """
+	def __init__(self, posTaggedSentences, newsStory: str):
+		"""
 			Constructor for the Data extractor class
 			Initializes the object
-        """
-        self.news = news
-        
-    def getLocation(self) -> str:
-        """
+		"""
+		self.news = newsStory
+		self.posTaggedSentences = posTaggedSentences
+
+		
+	def getLocation(self) -> str:
+		"""
 			Returns the location where the accident had happened
-        """
-        raise NotImplementedError
-    
-    def getDate(self):
-        """
+		"""
+		raise NotImplementedError
+	
+	def getDate(self):
+		"""
 			Returns the date of the accident
-        """
-        raise NotImplementedError
-    
-    def getDeathNumber(self):
-        raise NotImplementedError
-    
-    def getInjuryNumber(self):
-        raise NotImplementedError
-    
+		"""
+		raise NotImplementedError
+	
+	def getDeathNumber(self):
+		raise NotImplementedError
+	
+	def getInjuryNumber(self):
+		raise NotImplementedError
+	
