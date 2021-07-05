de72c9c70c49012bc6bb0049aa542e7db43b66c3
Sanskar Amgain <sanskaramgain@gmail.com>
2021-07-05 21:08:01 +0545

    Update admin.py, model registered

--- DeathExtraction/newsextraction/admin.py
+++ DeathExtraction/newsextraction/admin.py
@@ -1,3 +1,12 @@
+from newsextraction.models import rssdata
 from django.contrib import admin
-
 # Register your models here.
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["id","link","header","source","body","death","death_no","injury","injury_no","location","date","day","month","year","created_at","updated_at",]
+
+    #list_display_links = ["updated"]
+
+
+    class Meta:
+        model = rssdata
+admin.site.register(rssdata, PostModelAdmin)
