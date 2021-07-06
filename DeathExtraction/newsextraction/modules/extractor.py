9b4affdc083bd674ef96f0d8483ffceadb26677c
shawn00-tech <shawnjay001@gmail.com>
2021-07-06 11:59:55 +0545

    Update extractor.py
    Change getLocation

--- DeathExtraction/newsextraction/modules/extractor.py
+++ DeathExtraction/newsextraction/modules/extractor.py
@@ -2,6 +2,7 @@
 from .tagger import Tagger
 from .tokenizer import Tokenize
 import nltk
+from newsextraction.modules.locationTree import LocationInformation
 
 class DataExtractor:
 	def __init__(self, posTaggedSentences, newsStory: str):
@@ -17,7 +18,63 @@ class DataExtractor:
 		"""
 			Returns the location where the accident had happened
 		"""
-		raise NotImplementedError
+		individual_sentences = self.posTaggedSentences
+
+		locations = []
+
+		for sent in individual_sentences:
+			words = nltk.word_tokenize(sent)
+			if ("died" or "death" or "injured" or "injury" or "injuries" or "killed" or "casualty" or "accident" or "") in words:
+				#grouping similar kinds of words (name_entities)
+				chunked_sentence = nltk.ne_chunk(individual_sentences)
+				#label:GPE holds data for location in chunked_sentences
+				for i in chunked_sentence.subtrees(filter=lambda x: x.label() == 'GPE'):
+					print(i)
+					for i in i.leaves():
+						locations.append(i[0])
+
+		print("after extracting all locations : " + str(locations))
+
+		return_value = locations
+		try:
+				if (locations[0] == "New") or (locations[0] == "Old"):
+					return_value = []
+					return_value.append(locations[0] + " " + locations[1])
+		except:
+				pass
+
+		
+
+		
+		ktm_location = LocationInformation().all_ktm_locations()
+		bkt_location = LocationInformation().all_bkt_locations()
+		ltp_location = LocationInformation().all_ltp_locations()
+		outside_location = LocationInformation().all_locations()
+		all_locations = ktm_location + outside_location + bkt_location + ltp_location
+
+		locations_final = return_value
+		return_location = []
+		max_ratio = 0
+		max_location = []
+
+		for glocation in locations_final:
+			for location in all_locations:
+				dist = nltk.edit_distance(glocation, location)
+				ratio = (1 - (dist / len(glocation))) * 100
+				max_ratio = max(max_ratio, ratio)
+				if max_ratio >= 70:
+					max_location = location
+					if max_ratio == ratio:
+						if max_location in ktm_location:
+							return_location = max_location
+						elif max_location in ltp_location:
+							return_location = max_location
+						elif max_location in bkt_location:
+							return_location = max_location
+						elif max_location in outside_location:
+							return_location = max_location
+
+		return return_location
 	
 	def getDate(self, sentences):
 		""" Gets the number of deaths from the news story.
