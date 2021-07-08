9e1c3b22e3c7298d859e315b647a33a0034ab95d
shawn00-tech <shawnjay001@gmail.com>
2021-07-08 10:35:27 +0545

    Update extractor.py
    chabged  Data Extractor class

--- DeathExtraction/newsextraction/modules/extractor.py
+++ DeathExtraction/newsextraction/modules/extractor.py
@@ -14,6 +14,47 @@ class DataExtractor:
 		self.news = newsStory
 		self.posTaggedSentences = posTaggedSentences
 
+	
+	def get_title_location_chunks(self):
+		chunked = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(str(self.title).encode('ascii', errors='ignore').decode("utf-8"))))
+		prev = None
+		continuous_chunk = []
+		current_chunk = []
+
+		for subtree in chunked:
+			if type(subtree) == nltk.Tree and subtree.label() == 'GPE':
+				current_chunk.append(" ".join([token for token, pos in subtree.leaves()]))
+			if current_chunk:
+				named_entity = " ".join(current_chunk)
+				if named_entity not in continuous_chunk:
+					continuous_chunk.append(named_entity)
+					current_chunk = []
+			else:
+				continue
+		print(continuous_chunk)
+
+		return continuous_chunk
+
+	def get_body_location_chunks(self):
+		chunked = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(str(self.title).encode('ascii', errors='ignore').decode("utf-8"))))
+		prev = None
+		continuous_chunk = []
+		current_chunk = []
+
+		for subtree in chunked:
+			if type(subtree) == nltk.Tree and subtree.label() == 'GPE':
+				current_chunk.append(" ".join([token for token, pos in subtree.leaves()]))
+			if current_chunk:
+				named_entity = " ".join(current_chunk)
+				if named_entity not in continuous_chunk:
+					continuous_chunk.append(named_entity)
+					current_chunk = []
+			else:
+				continue
+		print(continuous_chunk)
+
+		return continuous_chunk
+
 		
 	def getLocation(self) :
 		"""
@@ -22,40 +63,48 @@ class DataExtractor:
 		"""
 
 		#search in title
-		title_sentences = nltk.sent_tokenize(str(self.title).encode('ascii', errors='ignore').decode("utf-8"))
+		# title_sentences = nltk.sent_tokenize(str(self.title).encode('ascii', errors='ignore').decode("utf-8"))
+		# locations =[]
+
+		# for sent in title_sentences:
+		# 	words = nltk.word_tokenize(sent)
+		# 	if ("died" or "death" or "injured" or "injury" or "injuries" or "killed" or "casualty" or "accident" or "crash" or "crashed" or "collided" or "hit" or "lost" ) in words:
+		# 		#grouping similar kinds of words (name_entities)
+		# 		chunked_sentence = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent)))
+		# 		#label:GPE holds data for location in chunked_sentences
+		# 		for i in chunked_sentence.subtrees(filter=lambda x: x.label() == 'GPE'):
+		# 			print(i)
+		# 			for i in i.leaves():
+		# 				locations.append(i[0])
+
+		# if len(locations) != 0:
+		# 	print("after extracting all locations : " + str(locations))
+			
+		# else:
+
+
+		# 	individual_sentences = nltk.sent_tokenize(str(self.news).encode('ascii', errors='ignore').decode("utf-8"))
+
+
+		# 	for sent in individual_sentences:
+		# 		words = nltk.word_tokenize(sent)
+		# 		if ("died" or "death" or "injured" or "injury" or "injuries" or "killed" or "casualty" or "accident" or "crash" or "crashed" or "collided" or "hit" or "lost" ) in words:
+		# 			#grouping similar kinds of words (name_entities)
+		# 			chunked_sentence = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent)))
+		# 			#label:GPE holds data for location in chunked_sentences
+		# 			for i in chunked_sentence.subtrees(filter=lambda x: x.label() == 'GPE'):
+		# 				print(i)
+		# 				for i in i.leaves():
+		# 					locations.append(i[0])
+			
 		locations =[]
-
-		for sent in title_sentences:
-			words = nltk.word_tokenize(sent)
-			if ("died" or "death" or "injured" or "injury" or "injuries" or "killed" or "casualty" or "accident" or "crash" or "crashed" or "collided" or "hit" or "lost" ) in words:
-				#grouping similar kinds of words (name_entities)
-				chunked_sentence = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent)))
-				#label:GPE holds data for location in chunked_sentences
-				for i in chunked_sentence.subtrees(filter=lambda x: x.label() == 'GPE'):
-					print(i)
-					for i in i.leaves():
-						locations.append(i[0])
-
-		if len(locations) != 0:
-			print("after extracting all locations : " + str(locations))
-			
+		locations=self.get_title_location_chunks()
+		if len(locations) == 0:
+			print(len(locations))
+			print("after extracting all locations from title : " + str(locations))
 		else:
-
-
-			individual_sentences = nltk.sent_tokenize(str(self.news).encode('ascii', errors='ignore').decode("utf-8"))
-
-
-			for sent in individual_sentences:
-				words = nltk.word_tokenize(sent)
-				if ("died" or "death" or "injured" or "injury" or "injuries" or "killed" or "casualty" or "accident" or "crash" or "crashed" or "collided" or "hit" or "lost" ) in words:
-					#grouping similar kinds of words (name_entities)
-					chunked_sentence = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent)))
-					#label:GPE holds data for location in chunked_sentences
-					for i in chunked_sentence.subtrees(filter=lambda x: x.label() == 'GPE'):
-						print(i)
-						for i in i.leaves():
-							locations.append(i[0])
-			
+			locations= self.get_body_location_chunks()
+			print("after extracting all locations from body: " + str(locations))
 
 		return_value = locations
 
