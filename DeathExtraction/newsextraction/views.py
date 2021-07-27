da3e72d23c1611458ded8156b73e72e2805917a8
Sanskar Amgain <sanskaramgain@gmail.com>
2021-07-27 09:25:20 +0545

    created a front end to work with django

--- DeathExtraction/newsextraction/views.py
+++ DeathExtraction/newsextraction/views.py
@@ -10,66 +10,42 @@ from .methods import *
 from django.db.models import Q #object used to encapsulate a collection of keyword arguments specified as in “Field lookups”.
 
 def index(request):
-    initial_check()
-    news_list = rssdata.objects.all().order_by("-date")
-    page = request.GET.get('page', 1)
+	initial_check()
+	news_list = rssdata.objects.all().order_by("-date")
+	page = request.GET.get('page', 1)
 
-    paginator = Paginator(news_list, 5)
-    try:
-        news = paginator.page(page)
-    except PageNotAnInteger:
-        news = paginator.page(1)
-    except EmptyPage:
-        news = paginator.page(paginator.num_pages)
-    return render(request, 'index.html', {'news': news})
+	paginator = Paginator(news_list, 5)
+	try:
+		news = paginator.page(page)
+	except PageNotAnInteger:
+		news = paginator.page(1)
+	except EmptyPage:
+		news = paginator.page(paginator.num_pages)
+	print(list(news_list)[0].date)
+	return render(request, 'newsextraction/index.html', {
+			'newsList': list(news_list),
+			'isHome':True,
+		}
+	)
 
 
 
 
 def extraction(request):
-    if request.method == 'POST':
-        form = NameForm(request.POST)
-
-        if form.is_valid():
-            data = form.cleaned_data
-            extracted_data = data['news_link']
-            link, news , title =  newstotext.story_extract(extracted_data)
-
-            #
-            # If you want to save the input news
-            oldlinks = rssdata.objects.values_list('link', flat=True)
-
-            if link not in oldlinks:
-                id = extract(link, news, title)
-                return render(request, 'extraction.html', {'form': form,
-                                                           'news_id': id,
-                                                           'article': rssdata.objects.get(pk=id)})
-            else:
-                id = 5
-                return render(request, 'extraction.html', {'form': form,
-                                                           'news_id': id,
-                                                           'article': rssdata.objects.get(link=link)})
-
-
-        
-
-    else:
-        form = NameForm()
-
-    return render(request, 'extraction.html', {'form': form})
+	return render(request, 'newsextraction/extraction.html', {'isHome':False})
 
 
 def about_us(request):
-    return render(request, 'about_us.html')
+	return render(request, 'about_us.html')
 
 
 def contact_us(request):
-    return render(request, 'contact_us.html')
+	return render(request, 'contact_us.html')
 
 def searchquery(request):
-    if request.POST:
-        query = request.POST.get('query', None)
-        querylist = rssdata.objects.values('body').filter(Q(body__icontains=query))
+	if request.POST:
+		query = request.POST.get('query', None)
+		querylist = rssdata.objects.values('body').filter(Q(body__icontains=query))
 
-        return render(request, 'searchquery.html',{'searchquery':querylist,
-                                                    'query':query,})
+		return render(request, 'searchquery.html',{'searchquery':querylist,
+													'query':query,})
