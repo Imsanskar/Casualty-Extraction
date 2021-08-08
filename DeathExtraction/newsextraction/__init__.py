cee992fb5715da849e37bf0a0d9a3f5e2ba50ffd
Sanskar Amgain <sanskaramgain@gmail.com>
2021-08-08 21:24:35 +0545

    nltk download added

--- DeathExtraction/newsextraction/__init__.py
+++ DeathExtraction/newsextraction/__init__.py
@@ -1,4 +1,15 @@
 from transformers import pipeline
-
+import nltk
 
 question_answerer = pipeline('question-answering')
+
+try:
+	nltk.data.find('tokenizers/punkt')
+except LookupError:
+	nltk.download("punkt")
+
+
+try:
+	nltk.data.find('tokenizers/punkt')
+except LookupError:
+	nltk.download('averaged_perceptron_tagger')
