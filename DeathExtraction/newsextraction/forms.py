e37b90233b798d8350bf10fbd2087b15542d8ad6
Sanskar Amgain <sanskaramgain@gmail.com>
2021-08-15 07:35:45 +0545

    Nameform updated

--- DeathExtraction/newsextraction/forms.py
+++ DeathExtraction/newsextraction/forms.py
@@ -4,4 +4,4 @@ class NameForm(forms.Form):
     """
     input: news link youu wnat to manually extract
     """
-    news_link = forms.CharField(max_length=500, label='Search', widget=forms.TextInput(attrs={'class':'form', 'placeholder':'Search'}))
+    news_link = forms.CharField(max_length=500, label='News Link', widget=forms.TextInput(attrs={'class':'form-control', 'id':"newsLinkForm", 'placeholder':'Search'}))
