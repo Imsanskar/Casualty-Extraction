43d9732ff4812c76bad292dcbe0537b86f8de636
Sanskar Amgain <sanskaramgain@gmail.com>
2021-08-15 07:44:22 +0545

    Merge conflict resolved

--- DeathExtraction/newsextraction/forms.py
+++ DeathExtraction/newsextraction/forms.py
@@ -5,3 +5,19 @@ class NameForm(forms.Form):
     input: news link youu wnat to manually extract
     """
     news_link = forms.CharField(max_length=500, label='News Link', widget=forms.TextInput(attrs={'class':'form-control', 'id':"newsLinkForm", 'placeholder':'Search'}))
+
+
+
+class SearchForm(forms.Form):
+    
+    all = forms.BooleanField(widget=forms.CheckboxInput(),initial=True,required=False)
+    header = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+    body = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+    source = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+    location = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+    death_no = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+    injury_no = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+    date = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+    year = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+    month = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+    day = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
--- DeathExtraction/newsextraction/urls.py
+++ DeathExtraction/newsextraction/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('graph/', views.graph, name='graph'),
     path('about_us/', views.about_us, name='about_us'),
     path('contact_us/', views.contact_us, name='contact_us'),
-    path('search/', views.searchquery, name='searchquery'),
+    path('search/', views.searchView, name='searchview'),
 ]
