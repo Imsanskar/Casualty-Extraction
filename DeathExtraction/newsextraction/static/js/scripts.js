e3c3a90fb0eee4bdc2c7cab294ad01dcfb8cb589
Sanskar Amgain <sanskaramgain@gmail.com>
2021-08-19 17:17:37 +0545

    Map added along with markers

--- DeathExtraction/newsextraction/static/js/scripts.js
+++ DeathExtraction/newsextraction/static/js/scripts.js
@@ -1,43 +1,18 @@
-
 // Scripts
 
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-        });
-    }
-
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Toggle the side navigation
+  const sidebarToggle = document.body.querySelector("#sidebarToggle");
+  if (sidebarToggle) {
+    sidebarToggle.addEventListener("click", (event) => {
+      event.preventDefault();
+      document.body.classList.toggle("sb-sidenav-toggled");
+      localStorage.setItem(
+        "sb|sidebar-toggle",
+        document.body.classList.contains("sb-sidenav-toggled")
+      );
+    });
+  }
 });
 
-
-
 // -----------map script---------------
-
-
-let mapview;
-
-function initmap() {
-    mapview = new google.maps.Map(document.getElementById("mapcontainer"), {
-        center: { lat: 27.7172, lng: 85.3240 },
-        zoom: 12,
-    });
-}
-
-// -------------------extractionpage js-----------------
-// let infoContainer = document.getElementById("infoContainer");
-// infoContainer.style.display ="none";
-// let submitbtn = document.getElementById("submitbtn");
-// submitbtn.addEventListener('click', event=>{
-//     infoContainer.style.display ="block";
-// })
-
-
-function test(l){
-	console.log(l)
-}
