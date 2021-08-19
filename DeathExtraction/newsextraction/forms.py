759ea1217dcad02b943e587fa48bca1dad75d766
Sanskar Amgain <sanskaramgain@gmail.com>
2021-08-19 17:59:39 +0545

    Extraction save added and duplicate filter added

--- DeathExtraction/newsextraction/forms.py
+++ DeathExtraction/newsextraction/forms.py
@@ -1,23 +1,23 @@
 
 from django import forms
 class NameForm(forms.Form):
-    """
-    input: news link youu wnat to manually extract
-    """
-    news_link = forms.CharField(max_length=500, label='News Link', widget=forms.TextInput(attrs={'class':'form-control', 'id':"newsLinkForm", 'placeholder':'Search'}))
-
+	"""
+	input: news link youu wnat to manually extract
+	"""
+	news_link = forms.CharField(max_length=500, label='News Link', widget=forms.TextInput(attrs={'class':'form-control', 'id':"newsLinkForm", 'placeholder':'Search'}))
+	isSave = forms.BooleanField(required=False)
 
 
 class SearchForm(forms.Form):
-    
-    all = forms.BooleanField(widget=forms.CheckboxInput(),initial=True,required=False)
-    header = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
-    body = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
-    source = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
-    location = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
-    death_no = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
-    injury_no = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
-    date = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
-    year = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
-    month = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
-    day = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+	
+	all = forms.BooleanField(widget=forms.CheckboxInput(),initial=True,required=False)
+	header = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+	body = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+	source = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+	location = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+	death_no = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+	injury_no = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+	date = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+	year = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+	month = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+	day = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
