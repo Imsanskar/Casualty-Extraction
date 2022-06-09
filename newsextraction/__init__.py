a9b822817a7560460369fe150615380ed316d05a
Sanskar Amgain <sanskaramgain@gmail.com>
2022-06-09 21:22:19 +0545

    nltk bug fixed

--- newsextraction/__init__.py
+++ newsextraction/__init__.py
@@ -5,13 +5,12 @@ import nltk
 try:
 	nltk.data.find('tokenizers/punkt')
 except LookupError:
-	nltk.data.find('tokenizers/averaged_perceptron_tagger')
 	nltk.download("punkt")
+	nltk.download('averaged_perceptron_tagger')
 
 
 # try:
 # 	nltk.data.find('tokenizers/averaged_perceptron_tagger')
 # except LookupError:
-# 	nltk.download('averaged_perceptron_tagger')
 
 question_answerer = pipeline('question-answering')
